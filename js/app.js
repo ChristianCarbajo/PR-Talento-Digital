329376cf0c26b6660ebd62a97d354e740e8b4ca8
Borja <elbarmandev@gmail.com>
2023-09-20 15:28:06 +0200

    include vid

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+const headerLogo = document.querySelector('.header-logo'); 
+
+//Abre el menú desplegable y oculta el texto
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    headerLogo.classList.toggle('hidden'); // Agrega o quita la clase hidden para ocultar la sección .header-logo
+});
